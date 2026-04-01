40170456af2e2a96a13c44cc123eb8e1f947cf50
Oleg Zalesskiy <ozalesskiy71@gmail.com>
2026-04-02 02:43:12 +0300

    setup

--- button/inline/inline.view.css.ts
+++ button/inline/inline.view.css.ts
@@ -5,12 +5,7 @@ namespace $.$$ {
 	$mol_style_define( $eve_button_inline, {
 		display: 'inline-flex',
 		verticalAlign: 'baseline',
-		padding: {
-			left: rem( 0.375 ),
-			right: rem( 0.375 ),
-			top: rem( 0.125 ),
-			bottom: rem( 0.125 ),
-		},
+		padding: rem(0),
 		borderRadius: rem( 0.25 ),
 		minHeight: 0,
 	} )
